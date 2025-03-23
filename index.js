78fec24cbd0e9b6cfcdba8493aac99af878f4ded
Ahmed Eldaly <96292450+ahmedaldaly@users.noreply.github.com>
2025-03-23 17:43:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const conectDB = require("./config/db");
-conectDB();
+const express = require('express')
+const app = express()
+const env = require('dotenv').config()
+const conectDB = require('./config/db')
+app.use(express.json());
+conectDB()
 
+
+// google
 const session = require("express-session");
 const passport = require("passport");
-
-// Middleware
-app.use(express.json());
 app.use(
-  session({
-    secret: "your_secret_key",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
+    session({
+      secret: "your_secret_key",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+  
+  // تشغيل Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.get("/", (req, res) => {
   res.send("Hello from Vercel!");
 });
-app.use("/api/v1/auth", require("./route/auth"));
-app.use("/api/v1/user", require("./route/user"));
-app.use("/api/v1/room", require("./route/room"));
-app.use("/api/v1/booking", require("./route/booking"));
-app.use("/api/v1/paypal", require("./route/paypal"));
 
-// ✅ تصدير `app` بدلاً من تشغيله مباشرة
-module.exports = app;
+
+
+app.use('/api/v1/auth',require('./route/auth'))
+app.use('/api/v1/user',require('./route/user'))
+app.use('/api/v1/room',require('./route/room'))
+app.use('/api/v1/booking',require('./route/booking'))
+app.use('/api/v1/paypal', require('./route/paypal'));
+app.listen(4000, () => {
+    console.log(`Server started on port`);
+});
